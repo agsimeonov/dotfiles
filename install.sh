5129da01f25b953def548df0ef4125d48a5d8e0b
Alexander Simeonov <agsimeon@buffalo.edu>
2016-04-20 02:10:46 -0400

    Minor improvements to install script.

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
-pushd `dirname $0` > /dev/null
-SCRIPTPATH=`pwd -P`
-popd > /dev/null
+# Zsh
 git clone git@github.com:agsimeonov/dotfiles.git ~/.dotfiles
 ln -s ~/.dotfiles/.zshenv ~/.
 ln -s ~/.dotfiles/.zshrc ~/.
 ln -s ~/.dotfiles/.zsh ~/.
+
+# Tmux
+ln -s ~/.dotfiles/.tmux.conf ~/.
