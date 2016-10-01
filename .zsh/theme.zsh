ce978f63b5f09b703ea4cb7074a51a208e12a622
Alexander Simeonov <agsimeon@buffalo.edu>
2016-10-01 01:53:21 -0400

    vi_mode for latest version of the perlevel9k theme.

--- .zsh/theme.zsh
+++ .zsh/theme.zsh
@@ -20,3 +20,32 @@ POWERLEVEL9K_VI_COMMAND_MODE_STRING="\ue20c"
 export DEFAULT_USER="$USER"
 
 source $ZSH_THEME_DIR/powerlevel9k.zsh-theme
+
+# 'zle-*' overrides - https://github.com/bhilburn/powerlevel9k/issues/319
+function zle-line-init {
+  powerlevel9k_prepare_prompts
+  if (( ${+terminfo[smkx]} )); then
+    printf '%s' ${terminfo[smkx]}
+  fi
+  zle reset-prompt
+  zle -R
+}
+
+function zle-line-finish {
+  powerlevel9k_prepare_prompts
+  if (( ${+terminfo[rmkx]} )); then
+    printf '%s' ${terminfo[rmkx]}
+  fi
+  zle reset-prompt
+  zle -R
+}
+
+function zle-keymap-select {
+  powerlevel9k_prepare_prompts
+  zle reset-prompt
+  zle -R
+}
+
+zle -N zle-line-init
+zle -N ale-line-finish
+zle -N zle-keymap-select
