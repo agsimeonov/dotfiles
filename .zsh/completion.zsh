0fc9489ba651fd2ed40d5df7b76322dd7e302a73
Alex Simeonov <asimeonov@trovedata.com>
2018-05-11 17:02:23 -0400

    Case insensitive tab completion and skipping extra tab for completion.

--- .zsh/completion.zsh
+++ .zsh/completion.zsh
@@ -6,3 +6,9 @@ zstyle ':completion:*' menu select
 
 # Handle specials dirs such as ..
 zstyle ':completion:*' special-dirs true
+
+# Case insensitive tab completion
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+
+# Tab completion starts without an extra tab
+setopt MENU_COMPLETE
