98110065592e79560a1c52c8d23338d175abe46b
Alexander Simeonov <agsimeonov@gmail.com>
2022-12-28 15:13:47 -0500

    Trimming some no longer used components.

--- install.sh
+++ install.sh
@@ -24,11 +24,6 @@ if ! [ -e $HOME/.vimrc ]; then
   ln -s $HOME/.dotfiles/.vimrc $HOME/.
 fi
 
-# Vrapper
-if ! [ -e $HOME/.vrapperrc ]; then
-  ln -s $HOME/.dotfiles/.vrapperrc $HOME/.
-fi
-
 # Postgres
 if ! [ -e $HOME/.psqlrc ]; then
   ln -s $HOME/.dotfiles/.psqlrc $HOME/.
@@ -37,26 +32,4 @@ fi
 # Interactive Shell
 if ! [ -e $HOME/.inputrc ]; then
   ln -s $HOME/.dotfiles/.inputrc $HOME/.
-fi
-
-# Counter-Strike Global Offensive
-if [ -d $HOME/Library/Application\ Support/Steam/steamapps/common/Counter-Strike\ Global\ Offensive/csgo/cfg/ ]; then
-  if ! [ -e $HOME/Library/Application\ Support/Steam/steamapps/common/Counter-Strike\ Global\ Offensive/csgo/cfg/autoexec.cfg ]; then
-    ln -s $HOME/.dotfiles/autoexec.cfg $HOME/Library/Application\ Support/Steam/steamapps/common/Counter-Strike\ Global\ Offensive/csgo/cfg/autoexec.cfg
-  fi
-fi
-
-# Streamlink
-if ! [ -e $HOME/.streamlinkrc ]; then
-  ln -s $HOME/.dotfiles/.streamlinkrc $HOME/.
-fi
-
-# Livestreamer
-if ! [ -e $HOME/.livestreamerrc ]; then
-  ln -s $HOME/.dotfiles/.livestreamerrc $HOME/.
-fi
-
-# Vim Plug
-if ! [ -e $HOME/.vim/autoload/plug.vim ]; then
-  curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
